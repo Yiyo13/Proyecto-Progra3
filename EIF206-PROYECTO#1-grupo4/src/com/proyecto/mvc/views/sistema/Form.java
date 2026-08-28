7322a89da9f5e17bb5cd9106f152e90f9fed22cf
Yiyo <geovanny.obando.olivas@est.una.ac.cr>
2026-08-27 22:13:11 -0600

    mas cambios en lel form principal y el index
    mas cambios

---
+++ EIF206-PROYECTO#1-grupo4/src/com/proyecto/mvc/views/sistema/Form.java
@@ -0,0 +1,80 @@
+package com.proyecto.mvc.views.sistema;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.Dimension;
+
+public class Form extends JPanel {
+	private JTextField textField;
+
+	/**
+	 * Create the panel.
+	 */
+	public Form() {
+		setBorder(new EmptyBorder(0, 0, 0, 0));
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(Color.WHITE);
+		panel.setBackground(Color.DARK_GRAY);
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		add(panel, BorderLayout.NORTH);
+		
+		JLabel lblTareasregistrar = new JLabel("Tareas-Registrar/Editar");
+		lblTareasregistrar.setForeground(Color.WHITE);
+		lblTareasregistrar.setBackground(Color.LIGHT_GRAY);
+		lblTareasregistrar.setBorder(new EmptyBorder(20, 20, 20, 20));
+		lblTareasregistrar.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblTareasregistrar.setHorizontalTextPosition(SwingConstants.LEFT);
+		lblTareasregistrar.setHorizontalAlignment(SwingConstants.LEFT);
+		panel.add(lblTareasregistrar);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EmptyBorder(40, 40, 40, 40));
+		add(panel_1, BorderLayout.WEST);
+		panel_1.setLayout(new GridLayout(10, 1, 0, 10));
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setHorizontalTextPosition(SwingConstants.LEFT);
+		lblNombre.setHorizontalAlignment(SwingConstants.LEFT);
+		panel_1.add(lblNombre);
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblDescripcin = new JLabel("Descripci\u00F3n");
+		panel_1.add(lblDescripcin);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setMinimumSize(new Dimension(20, 20));
+		panel_1.add(textArea);
+		
+		JLabel lblCategora = new JLabel("Categor\u00EDa");
+		panel_1.add(lblCategora);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccione una"}));
+		panel_1.add(comboBox);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		panel_1.add(btnGuardar);
+		
+		JButton btnCancekar = new JButton("Cancelar\r\n");
+		panel_1.add(btnCancekar);
+
+	}
+
+}
---
+++ EIF206-PROYECTO#1-grupo4/src/com/proyecto/mvc/views/sistema/Index.java
@@ -0,0 +1,66 @@
+package com.proyecto.mvc.views.sistema;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class Index extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Index() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JLabel lblTareasPendientes = new JLabel("Tareas Pendientes");
+		lblTareasPendientes.setMaximumSize(new Dimension(106, 40));
+		lblTareasPendientes.setPreferredSize(new Dimension(135, 60));
+		lblTareasPendientes.setForeground(Color.WHITE);
+		lblTareasPendientes.setBackground(Color.WHITE);
+		panel.add(lblTareasPendientes);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panel.add(panel_1);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblCategora = new JLabel("Categor\u00EDa");
+		lblCategora.setVerticalTextPosition(SwingConstants.BOTTOM);
+		lblCategora.setVerticalAlignment(SwingConstants.BOTTOM);
+		panel_1.add(lblCategora);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setPreferredSize(new Dimension(300, 22));
+		panel_1.add(comboBox);
+		
+		JButton btnCargar = new JButton("Cargar");
+		panel_1.add(btnCargar);
+		
+		JPanel panel_2 = new JPanel();
+		add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		panel_2.add(panel_3, BorderLayout.NORTH);
+		
+		JLabel lblCategora_1 = new JLabel("Categor\u00EDa");
+		lblCategora_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblCategora_1.setHorizontalTextPosition(SwingConstants.LEFT);
+		panel_3.add(lblCategora_1);
+
+	}
+
+}
