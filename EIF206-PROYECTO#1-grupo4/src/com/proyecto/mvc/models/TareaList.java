817da4b4886351a5a5b3243086212c90fbc91806
j0sMedina <jos.mathiasmg@gmail.com>
2026-08-30 13:29:28 -0600

    Implementacion del CRUD basico

---
+++ EIF206-PROYECTO#1-grupo4/src/com/proyecto/mvc/models/TareaList.java
@@ -0,0 +1,64 @@
+package com.proyecto.mvc.models;
+
+import java.util.ArrayList;
+
+public class TareaList {
+
+	private ArrayList<Tarea> list;
+	
+	public TareaList() {
+		list = new ArrayList<>();
+	}
+	
+	// OPERACIONES
+
+	public void store(Tarea item) {
+		list.add(item);
+	}
+
+	public void update(Tarea updated, int id) {
+		Tarea item = find(id);
+		if (item != null) {
+			item.setNombre(updated.getNombre());
+			item.setDescripcionT(updated.getDescripcionT());
+			item.setCompletada(updated.isCompletada());
+			item.setIdCategoria(updated.getIdCategoria());
+		}
+	}
+
+	public void destroy(int id) {
+		Tarea item = find(id);
+		if (item != null) {
+			list.remove(item);
+		}
+	}
+
+	// CONSULTAS
+
+	public Tarea find(int id) {
+		return list
+				.stream()
+				.filter(v -> v.getId() == id)
+				.findFirst()
+				.orElse(null);
+	}
+
+	public ArrayList<Tarea> all() {
+		return list;
+	}
+	
+public int countByIdCategoria(int idCategoria) {
+		
+		int count = 0;
+		
+		for (Tarea item : list) {
+			if(item.getIdCategoria() == idCategoria) {
+			count++;
+			}
+		}
+		
+		return count;
+		
+	}
+
+}
